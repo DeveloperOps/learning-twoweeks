7f61af6ce1fe6b54cf46d8570712045d63b134f3
DeveloperOps <manidecp@gmail.com>
2021-01-05 10:47:58 +0530

    working with arrays and rendering data using loops in vue

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 new Vue({
   el: '#heading',
   data: {
-    heading: "Heading"
+    heading: "My list",
+    items: [
+      "Day 1 of vue js",
+      "I'm working with loops in vue",
+      "That how to render values on screen"
+    ]
   }
 });
